58df1ded22a2f896832714fd414adc3509bee01c
Gabriel DeFazio <aloveleter@gmail.com>
2017-11-18 08:51:19 -0500

    [FEATURE] added login

--- configureStore.js
+++ configureStore.js
@@ -2,12 +2,13 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { reducer as formReducer } from 'redux-form';
 import auth from './src/modules/auth';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducers = combineReducers({
   auth,
   form: formReducer,
 });
 
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 export default store;
