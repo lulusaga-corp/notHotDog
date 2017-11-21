f2db765aadadcf4add92adf378fb79d8d4179cf0
kapicaluk <kapicaluk@gmail.com>
2017-11-21 14:43:17 -0500

    loads chart in AccountHome, recives parsed object

--- src/scenes/AccountHome.js
+++ src/scenes/AccountHome.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-
 import {
     Image,
-    StyleSheet,
     Text,
+    StyleSheet,
     View
 } from 'react-native';
 import IconContainer from '../components/IconContainer';
+import { VictoryPie, VictoryLabel } from 'victory-native';
 
 //import AccountSettings from '../containers/LandingContainer';
 
 class AccountHome extends Component {
     render () {
-        return (
-            <View style={styles.tabContainer}>
-                <IconContainer />
-                <Text>Hello from Home</Text>
-            </View>
-        )
+        console.log('parsed stuff', this.props.parsed)
+        const nutrients = this.props.parsed
+        const serving = this.props.parsed.serving_weight_grams 
+        const data = {[{ cholesterol: , y: nutrients[nf_cholesterol] * 100 / 28000 }, { x: 'potassium', y: 21.28 * 100 / 28000 }, { x: 'sodium', y: 182.84 * 100 / 28000 }, { x: 'phat', y: 5.28 * 100 / 28000 }, { x: 'fat', y: 5.28 * 100 / 28000 }]}
+        return (<View style={styles.container}>
+            <IconContainer />
+            <VictoryPie colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']} data={data} />
+          </View>)
     }
 }
 
--- src/scenes/FoodSelector.js
+++ src/scenes/FoodSelector.js
@@ -1,94 +1,108 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import { connect } from 'react-redux'
-import { Actions } from 'react-native-router-flux'
-import { List, ListItem, Button } from 'react-native-elements';
-
+import React, { Component } from "react";
+import axios from "axios";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { connect } from "react-redux";
+import { Actions } from "react-native-router-flux";
+import { List, ListItem, Button } from "react-native-elements";
 
 class FoodSelector extends Component {
-    constructor (props){
-        super(props);
-        this.state = {
-          response: this.props.response,
-          foodInput:''
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: this.props.response,
+      foodInput: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleSubmit(e) {
-      axios.post('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-        query: this.state.response.join(", ")
-      }, {
-        headers: {
-          "x-app-id": "da40e3ba",
-          "x-app-key": "9039730dc95644122941bec700a3ebe4",
-          "Content-Type": "application/json"
+  handleSubmit(e) {
+    axios
+      .post(
+        "https://trackapi.nutritionix.com/v2/natural/nutrients",
+        {
+          query: this.state.response.join(", ")
+        },
+        {
+          headers: {
+            "x-app-id": "da40e3ba",
+            "x-app-key": "9039730dc95644122941bec700a3ebe4",
+            "Content-Type": "application/json"
+          }
         }
-      })
+      )
       .then(res => res.data)
-      .then(data => console.log("DATA", data))
-        .then(()=>Actions.AccountHome())
-    }
+      .then(data => {
+        let keys = Object.keys(data.foods[0]).slice(0, 17);
+        return data.foods.map(food => {
+          return keys.reduce((acc, cur) => {
+            acc[cur] = food[cur];
+            return acc;
+          }, {});
+        });
+      })
+      .then(parsed => Actions.AccountHome({parsed}))
+  }
 
-    render () {
-      console.log('state', this.state)
-        const filtered = this.state.response
-        return(
-            <View style={styles.tabContainer}>
-              <ScrollView>
-                <List>
-                    {
-                    filtered && filtered.map((item, i) => {
-                        return <ListItem key={i} title={item} rightIcon={{name: 'clear'}} onPressRightIcon={item => {
-                            let stateArr = this.state.response.slice()
-                            stateArr.splice(i, 1)
-                            this.setState({ response : stateArr })
-                        }} />
-                    })
-                    }
-                    <ListItem textInput={true}
-                              textInputValue={this.state.foodInput}
-                              textInputOnChangeText={(text)=>{
-                                this.setState({foodInput:text})
-                              }}
-                              textInputPlaceholder={'Add other foods...'}
-                              rightIcon={{ name: 'add'}}
-                              textInputAutoCorrect={true}
-                              textInputAutoCapitalize={"none"}
-                              onPressRightIcon={()=>{
-                                let stateArr = this.state.response.slice()
-                                stateArr.push(this.state.foodInput)
-                                this.setState({ response : stateArr })
-                                this.setState({foodInput:''})
-                              }}/>
-                    <ListItem 
-                      onPress={this.handleSubmit} 
-                      title="Click here to submit!"
-                      hideChevron={true} 
-                      />
-
-                </List>
-              </ScrollView>
-            </View>
-        )
-    }
+  render() {
+    console.log("state", this.state);
+    const filtered = this.state.response;
+    return (
+      <View style={styles.tabContainer}>
+        <ScrollView>
+          <List>
+            {filtered &&
+              filtered.map((item, i) => {
+                return (
+                  <ListItem
+                    key={i}
+                    title={item}
+                    rightIcon={{ name: "clear" }}
+                    onPressRightIcon={item => {
+                      let stateArr = this.state.response.slice();
+                      stateArr.splice(i, 1);
+                      this.setState({ response: stateArr });
+                    }}
+                  />
+                );
+              })}
+            <ListItem
+              textInput={true}
+              textInputValue={this.state.foodInput}
+              textInputOnChangeText={text => {
+                this.setState({ foodInput: text });
+              }}
+              textInputPlaceholder={"Add other foods..."}
+              rightIcon={{ name: "add" }}
+              textInputAutoCorrect={true}
+              textInputAutoCapitalize={"none"}
+              onPressRightIcon={() => {
+                let stateArr = this.state.response.slice();
+                stateArr.push(this.state.foodInput);
+                this.setState({ response: stateArr });
+                this.setState({ foodInput: "" });
+              }}
+            />
+            <ListItem
+              onPress={this.handleSubmit}
+              title="Click here to submit!"
+              hideChevron={true}
+            />
+          </List>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    tabContainer: {
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'column'
-    }
+  tabContainer: {
+    backgroundColor: "#fff",
+    flex: 1,
+    flexDirection: "column"
+  }
 });
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   response: state.food.foodArr
 });
 
