e3d811fc344d1e980440fb55d0b1fe6d343a3447
Sarah Katz <soshasarah@gmail.com>
2017-11-21 15:42:41 -0500

    cleaned up code

--- src/scenes/History.js
+++ src/scenes/History.js
@@ -1,51 +1,49 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
 } from 'react-native';
 import IconContainer from '../components/IconContainer';
 import { Card } from 'react-native-elements';
 
-// import HistoryContainer from '../containers/LandingContainer';
-
 class History extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            meals: [{date: 'june 30', food: 'potatoes'}, {date: 'june 30', food: 'potatoes'}, {date: 'june 30', food: 'potatoes'},{date: 'june 30', food: 'potatoes'}]
-        }
+  constructor(props){
+    super(props)
+    this.state = {
+      meals: [{date: 'june 30', food: 'potatoes'}, {date: 'june 30', food: 'potatoes'}, {date: 'june 30', food: 'potatoes'},{date: 'june 30', food: 'potatoes'}]
     }
-    render () {
-        const meals = this.state.meals;
-        return (
-            <View style={styles.tabContainer}>
-                <IconContainer />
-                <View style={styles.mealContainer}>
-                    {
-                        meals.map((meal, index) => {
-                            return (
-                                <TouchableOpacity key={index} onPress={() => console.log('preseeddd')}>
-                                    <Card title={meal.date} >
-                                        <Text>{meal.food}</Text>
-                                    </Card>
-                                </TouchableOpacity>
-                            )
-                        })
-                    }
-                </View>
-            </View>
-        )
-    }
+  }
+  render () {
+    const meals = this.state.meals;
+    return (
+      <View style={styles.tabContainer}>
+        <IconContainer />
+        <View style={styles.mealContainer}>
+          {
+            meals.map((meal, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => console.log('preseeddd')}>
+                  <Card title={meal.date} >
+                    <Text>{meal.food}</Text>
+                  </Card>
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    tabContainer: {
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'column'
-    }
+  tabContainer: {
+    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'column'
+  }
 });
 
 export default History;
