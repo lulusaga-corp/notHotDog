e4e417f9b0e08d132903205fa3de4a871f282983
Lucy Sutton <Theodore@Lucys-Mac-mini.local>
2017-11-21 15:44:11 -0500

    working on graph

--- src/scenes/AccountHome.js
+++ src/scenes/AccountHome.js
@@ -11,15 +11,44 @@ import { VictoryPie, VictoryLabel } from 'victory-native';
 //import AccountSettings from '../containers/LandingContainer';
 
 class AccountHome extends Component {
+    
     render () {
-        console.log('parsed stuff', this.props.parsed)
-        const nutrients = this.props.parsed
-        const serving = this.props.parsed.serving_weight_grams 
-        const data = {[{ cholesterol: , y: nutrients[nf_cholesterol] * 100 / 28000 }, { x: 'potassium', y: 21.28 * 100 / 28000 }, { x: 'sodium', y: 182.84 * 100 / 28000 }, { x: 'phat', y: 5.28 * 100 / 28000 }, { x: 'fat', y: 5.28 * 100 / 28000 }]}
-        return (<View style={styles.container}>
-            <IconContainer />
-            <VictoryPie colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']} data={data} />
-          </View>)
+        // console.log('parsed stuff', this.props.parsed && this.props.parsed[0].nf_cholesterol)
+        let data;
+        if(this.props.parsed){
+            const nutrients = this.props.parsed[0]
+            const serving = this.props.parsed[0].serving_weight_grams
+            console.log('serving', serving)
+            let cholestersol = ( Number(nutrients.nf_cholesterol) / 1000 ) / serving
+            console.log(cholestersol)
+            let sodium = ( Number(nutrients.nf_sodium) / 1000 ) / serving            
+            data = [ 
+                { x: 'cholestersol', y: cholestersol }, 
+                // { x: 'sodium', y: ((Number(nutrients.nf_sodium)/1000) * 100 / serving) }, 
+                // { x: 'total fat', y: ( nutrients.nf_total_fat * 100 / serving ) }, 
+                // { x: 'protein', y: ( nutrients.nf_protein * 100 / serving ) * 100 / serving }, 
+                // { x: 'dietary fiber', y: ( nutrients.nf_dietary_fiber * 100 / serving ) * 100 / serving },
+                // { x: 'sugar', y: ( nutrients.nf_sugars * 100 / serving ) * 100 / serving }
+            ]
+            // data = [ 
+            //     { x: 'cholestersol', y: 45 }, 
+            //     { x: 'sodium', y: 65 }, 
+            //     // { x: 'total fat', y: ( nutrients.nf_total_fat * 100 / serving ) }, 
+            //     // { x: 'protein', y: ( nutrients.nf_protein * 100 / serving ) * 100 / serving }, 
+            //     // { x: 'dietary fiber', y: ( nutrients.nf_dietary_fiber * 100 / serving ) * 100 / serving },
+            //     // { x: 'sugar', y: ( nutrients.nf_sugars * 100 / serving ) * 100 / serving }
+            // ]
+        }
+        return (
+            <View style={styles.container}>
+                <IconContainer />
+                {
+                    data && <VictoryPie colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy', 'pink']} data={data} />
+                }
+                {/* <Text>Here</Text> */}
+                
+            </View>
+        )
     }
 }
 
