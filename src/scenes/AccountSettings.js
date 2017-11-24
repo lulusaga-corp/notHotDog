a63ef9005efd9123cabe667709c9c3dc2aa30509
Sarah Katz <soshasarah@gmail.com>
2017-11-23 20:57:26 -0500

    updated info goes to firebase

--- src/scenes/AccountSettings.js
+++ src/scenes/AccountSettings.js
@@ -10,10 +10,16 @@ import {
 import { connect } from 'react-redux'
 import { signOutUser } from '../modules/auth'
 import { CheckBox, List, ListItem, FormLabel, FormInput } from 'react-native-elements'
+import firebase from 'firebase';
+import 'firebase/firestore';
+import {Actions} from 'react-native-router-flux';
 
 class AccountSettings extends Component {
   state = {
     user: this.props.user,
+    uid: this.props.uid,
+    firstname: this.props.user.displayName,
+    lastname: '',
     diet: {
       vegan: false,
       vegetarian: false,
@@ -21,7 +27,21 @@ class AccountSettings extends Component {
     },
     allergies: [],
     newAllergy: '',
-    editAccount: false
+    editAccount: false,
+    editDiet: false
+  }
+
+  componentDidMount(){
+    firebase.firestore().collection(`users`).doc(`${this.state.uid}`).get()
+    .then(res => res.data())
+    .then(data => {
+      this.setState({
+        diet: data.dietary.specialDiet || this.state.diet,
+        allergies: data.dietary.allergies || this.state.allergies,
+        firstname: data.name.firstname || this.state.firstname,
+        lastname: data.name.lastname || this.state.lastname
+      })
+    })
   }
 
   addVegan(){
@@ -51,8 +71,12 @@ class AccountSettings extends Component {
     })
   }
 
-  editName(text){
-    this.setState({user: {...this.state.user, displayName: text}})
+  editFirstName(text){
+    this.setState({user: {...this.state.user, displayName: text}, firstname: text})
+  }
+
+  editLastName(text){
+    this.setState({ lastname: text })
   }
 
   editEmail(text){
@@ -60,7 +84,47 @@ class AccountSettings extends Component {
   }
 
   editAccount(){
+    let updatedInfo = {
+      displayName: this.state.user.displayName,
+      email: this.state.user.email,
+    }
+    if (this.state.editAccount) {
+      let user = firebase.auth().currentUser
+      user.updateProfile({ displayName: `${updatedInfo.displayName}` })
+      .then(() => {
+        return user.updateEmail(`${updatedInfo.email}`)
+      })
+      .catch(err => console.error(err))
+    }
     this.setState({editAccount: !this.state.editAccount})
+  }
+
+  editDiet(){
+    let data = {
+      dietary: {
+        specialDiet: this.state.diet,
+        allergies: this.state.allergies
+      },
+      name: {
+        firstname: this.state.firstname,
+        lastname: this.state.lastname
+      }
+    }
+    if (this.state.editDiet) {
+      firebase.firestore().collection(`users`).doc(`${this.state.uid}`).set(data)
+      .catch(err => console.error(err))
+    }
+    this.setState({editDiet: !this.state.editDiet})
+  }
+
+  deleteAccount(){
+    let user = firebase.auth().currentUser
+    user.delete()
+    .then(() => {
+      return this.props.signOutUser()
+    })
+    .then(() => Actions.signup())
+    .catch(err => console.error(err))
   }
 
   render () {
@@ -75,99 +139,131 @@ class AccountSettings extends Component {
       <View>
         <View>
           <Text>Account Information:</Text>
-          {
-            this.state.editAccount ?
+          { this.state.editAccount ?
+            <View>
               <View>
-                <View>
-                  <FormLabel>Name:</FormLabel>
-                  <FormInput onChangeText={(text) => this.editName(text)} defaultValue={user.displayName} />
-                  <FormLabel>Email:</FormLabel>
-                  <FormInput onChangeText={(text) => this.editEmail(text)} defaultValue={user.email} />
-                </View>
-                <View>
-                  <Text>Dietary Preferences: (Check All That Apply)</Text>
-                  <CheckBox
-                    title='Vegan'
-                    checked={this.state.diet.vegan}
-                    onPress={this.addVegan.bind(this)}
-                  />
-                  <CheckBox
-                    title='Vegetarian'
-                    checked={this.state.diet.vegetarian}
-                    onPress={this.addVegetarian.bind(this)}
-                  />
-                  <CheckBox
-                    title='Gluten Free'
-                    checked={this.state.diet.gf}
-                    onPress={this.addgf.bind(this)}
-                  />
-                </View>
-                <ScrollView>
-                  <Text>Allergies:</Text>
-                  <List>
-                    {
-                    this.state.allergies && this.state.allergies.map((allergy, i) => {
-                      return (
-                        <ListItem 
-                          key={i} title={allergy} 
-                          rightIcon={{name: 'clear'}}
-                          onPressRightIcon={() => this.removeAllergy(allergy)}
-                        />
-                        )
-                      })
-                    }
-                    <ListItem
-                      textInput={true}
-                      textInputValue={this.state.newAllergy}
-                      textInputOnChangeText={(text) => this.allergyInput(text)}
-                      textInputPlaceholder={'Add an allergy ...'}
-                      rightIcon={{ name: 'add'}}
-                      textInputAutoCorrect={true}
-                      textInputAutoCapitalize={"none"}
-                      onPressRightIcon={() => this.addAllergy(this.state.newAllergy)} />
-                  </List>
-                </ScrollView>
-                <Button onPress={this.editAccount.bind(this)} title="Save Account Changes"/>
+                <FormLabel>First Name:</FormLabel>
+                <FormInput 
+                  onChangeText={(text) => this.editFirstName(text)} 
+                  defaultValue={(this.state.firstname) ? this.state.firstname : user.displayName} />
+                <FormLabel>Last Name:</FormLabel>
+                <FormInput 
+                  onChangeText={(text) => this.editLastName(text)} 
+                  defaultValue={(this.state.lastname) ? this.state.lastname : null} />
+                <FormLabel>Email:</FormLabel>
+                <FormInput 
+                  onChangeText={(text) => this.editEmail(text)} 
+                  defaultValue={user.email} />
               </View>
+              <Button onPress={this.editAccount.bind(this)} title="Save Account Changes"/>
+            </View>
+          :
+            <View>
+              <View>
+                <Text>Name: {this.state.firstname || user.displayName || null} {this.state.lastname || null} </Text>
+                <Text>Email: {user.email} </Text>
+              </View>
+              <Button onPress={this.editAccount.bind(this)} title="Edit Account Info"/>
+            </View>
+          }
+        </View>
+        <View>
+        { this.state.editDiet ?
+          <View>
+            <View>
+              <Text>Dietary Preferences: (Check All That Apply)</Text>
+              <CheckBox
+                title='Vegan'
+                checked={this.state.diet.vegan}
+                onPress={this.addVegan.bind(this)}
+              />
+              <CheckBox
+                title='Vegetarian'
+                checked={this.state.diet.vegetarian}
+                onPress={this.addVegetarian.bind(this)}
+              />
+              <CheckBox
+                title='Gluten Free'
+                checked={this.state.diet.gf}
+                onPress={this.addgf.bind(this)}
+              />
+            </View>
+              <ScrollView>
+                <Text>Allergies:</Text>
+                <List>
+                  {
+                  this.state.allergies && this.state.allergies.map((allergy, i) => {
+                    return (
+                      <ListItem 
+                        key={i} title={allergy} 
+                        rightIcon={{name: 'clear'}}
+                        onPressRightIcon={() => this.removeAllergy(allergy)}
+                      />
+                      )
+                    })
+                  }
+                  <ListItem
+                    textInput={true}
+                    textInputValue={this.state.newAllergy}
+                    textInputOnChangeText={(text) => this.allergyInput(text)}
+                    textInputPlaceholder={'Add an allergy ...'}
+                    rightIcon={{ name: 'add'}}
+                    textInputAutoCorrect={true}
+                    textInputAutoCapitalize={"none"}
+                    onPressRightIcon={() => this.addAllergy(this.state.newAllergy)} />
+                </List>
+              </ScrollView>
+              <Button onPress={this.editDiet.bind(this)} title="Save Special Diet Changes"/>
+            </View>
             :
-              <View>
+            <View>
+              { diet.length > 0 && 
                 <View>
-                  <Text>Name: {user.displayName || null} </Text>
-                  <Text>Email: {user.email} </Text>
+                  <Text>Special Diet:</Text>
+                  {diet.map((diet, i) => {
+                    return (
+                      <Text key={i}>{diet} </Text>
+                      )
+                    })
+                  }
                 </View>
-                { diet.length > 0 && 
-                  <View>
-                    <Text>Special Diet:</Text>
-                    {diet.map((diet, i) => {
-                      return (
-                        <Text key={i}>{diet} </Text>
-                        )
-                      })
-                    }
-                  </View>
-                }
-                {
-                  this.state.allergies.length > 0 && <View>
-                    <Text>Allergies</Text>
-                    {
-                      this.state.allergies.map((allergy, i) => {
-                      return (
-                        <Text key={i}> {allergy} </Text>
-                        )
-                      })
-                    }
-                  </View>
-                }
-                <Button onPress={this.editAccount.bind(this)} title="Edit Account Info"/>
-              </View>
+              }
+              {
+                this.state.allergies.length > 0 && <View>
+                  <Text>Allergies</Text>
+                  {
+                    this.state.allergies.map((allergy, i) => {
+                    return (
+                      <Text key={i}> {allergy} </Text>
+                      )
+                    })
+                  }
+                </View>
+              }
+              <Button onPress={this.editDiet.bind(this)} title="Edit Special Diet Info"/>
+            </View>
           }
         </View>
         <View>
           <Button onPress={ this.props.signOutUser } title="SignOut"/>
+          <Button onPress={ this.deleteAccount.bind(this) } title="Delete My Account"/>
         </View>
       </View>
     )
   }
 }
 
-export default connect( (state)=>({user: state.auth.user.providerData[0]}), {signOutUser} )(AccountSettings);
+const mapStateToProps = state => {
+  if (state.auth.user) {
+    return {
+      user: state.auth.user.providerData[0],
+      uid: state.auth.user.uid
+    }
+  } else {
+    return {
+      user: {}
+    }
+  }
+}
+
+export default connect( mapStateToProps, {signOutUser} )(AccountSettings);
