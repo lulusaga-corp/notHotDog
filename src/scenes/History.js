4ac41d02a3e61150684ada4c2ab0283ff3cb9ca1
Gabriel DeFazio <aloveleter@gmail.com>
2017-11-25 01:50:07 -0500

    History component renders meal date and foods of each meal

--- src/scenes/History.js
+++ src/scenes/History.js
@@ -12,15 +12,10 @@ import { connect } from 'react-redux'
 class History extends Component {
   constructor(props){
     super(props)
-    this.state = {
-      meals: [{date: 'june 30', food: 'potatoes'}, {date: 'june 30', food: 'potatoes'}, {date: 'june 30', food: 'potatoes'},{date: 'june 30', food: 'potatoes'}]
-    }
   }
 
   render () {
     const { allMeals } = this.props
-    const {meals} = this.state
-    console.log("allmeals", this.props.allMeals)
 
     return (
       <View style={styles.tabContainer}>
@@ -28,8 +23,6 @@ class History extends Component {
         <View style={styles.mealContainer}>
           {
             allMeals && allMeals.map((meal, index) => {
-              console.log('meal from map',meal)
-              console.log("meal", meal)
               return (
                 <TouchableOpacity key={index} onPress={() => console.log('preseeddd')}>
                   <Card title={meal.timestamp.toString(' ').split(' ').slice(0,5).join(' ')} >
