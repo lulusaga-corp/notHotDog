11ebdba4bf83a1cc899c08bf07ba8f88a775b15e
kapicaluk <kapicaluk@gmail.com>
2017-11-26 22:53:26 -0500

    added legend

--- src/scenes/PieChart.js
+++ src/scenes/PieChart.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
-import { Image, Text, StyleSheet, View, ScrollView } from "react-native";
+import { Image, Text, StyleSheet, View, ScrollView, Dimensions } from "react-native";
 import IconContainer from "../components/IconContainer";
 import {
   VictoryPie,
   VictoryLabel,
   VictoryContainer,
-  VictoryChart
+  VictoryChart,
+  VictoryLegend
 } from "victory-native";
 import { Svg } from "react-native-svg";
+import { style } from "expo/src/Font";
 
 class PieChart extends Component {
   render() {
-    let allFoods = this.props.allFoods
+    let allFoods = this.props.allFoods;
     let foodCharts = [];
     if (allFoods) {
       allFoods.forEach(food => {
@@ -27,8 +29,7 @@ class PieChart extends Component {
     }
 
     return (
-      <ScrollView>
-        <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
           {foodCharts &&
             foodCharts.map((food, idx) => {
               let chartData = [];
@@ -38,34 +39,49 @@ class PieChart extends Component {
                 { x: "protein", y: food.data.protein }
               );
               return (
-                <View key={idx} style={styles.graphBox}>
-                  <Text> {food.food_name} </Text>
-                  <Svg viewBox={"0 0 100, 100"}>
-                    <VictoryPie
-                      colorScale={["tomato", "orange", "gold"]}
-                      data={chartData}
-                      innerRadius={3}
-                      padAngle={2}
-                    />
-                  </Svg>
+                <View key={idx} style={styles.container}>
+                  <Svg width={Dimensions.get("window").width} height={130}>
+                    <VictoryLegend x={100} y={100}
+                    centerTitle
+                    orientation="horizontal"
+                    gutter={10}
+                    data={[
+                      { name: "Carbs", symbol: { fill: "tomato" } },
+                      { name: "Fat", symbol: { fill: "orange" } },
+                      { name: "Protein", symbol: { fill: "gold" } }
+                    ]}
+                  />
+                </Svg>
+                <VictoryPie
+                colorScale={["tomato", "orange", "gold"]}
+                data={chartData}
+                innerRadius={2}
+                padAngle={2}
+                style={{ labels: { fontSize: 1 } }}
+                />
+                <Text style={styles.title}>{food.food_name}</Text>
                 </View>
               );
             })}
-        </View>
-      </ScrollView>
+        </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 50,
+    paddingRight: 50,
   },
-  graphBox: {
-    flex: 1,
-    alignItems: "center"
+  title: {
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 18,
+    fontWeight: "bold",
   }
+  
 });
 
 export default PieChart;
-
