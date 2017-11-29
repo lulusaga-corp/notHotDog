eda27819e5fc1c17640d4eb5399000d71f9afc39
Sarah Katz <soshasarah@gmail.com>
2017-11-29 12:35:47 -0500

    added daily values to nutrition parser

--- src/utilities/nutrientParser.js
+++ src/utilities/nutrientParser.js
@@ -26,23 +26,23 @@ let key = {
     269: ['Sugars', g],
     287: ['Galactose', g],
     291: ['Dietary Fiber', g],
-    301: ['Calcium', mg],
-    303: ['Iron', mg],
-    304: ['Magnesium', mg],
-    305: ['Phosphorus', mg],
-    306: ['Potassium', mg],
-    307: ['Sodium', mg],
-    309: ['Zinc', mg],
-    312: ['Copper', mg],
+    301: ['Calcium', mg, 1000],
+    303: ['Iron', mg, 18],
+    304: ['Magnesium', mg, 400],
+    305: ['Phosphorus', mg, 1000],
+    306: ['Potassium', mg, 3500],
+    307: ['Sodium', mg, 2400],
+    309: ['Zinc', mg, 15],
+    312: ['Copper', mg, 2],
     313: ['Fluoride', Âµg],
-    315: ['Manganese', mg],
-    317: ['Selenium', Âµg],
-    318: ['Vitamin A', IU],
+    315: ['Manganese', mg, 2],
+    317: ['Selenium', Âµg, 70],
+    318: ['Vitamin A', IU, 5000],
     319: ['Retinol', Âµg],
     321: ['Beta Carotene', Âµg],
     322: ['Alpha Carotene', Âµg],
-    323: ['Vitamin E', mg],
-    324: ['Vitamin D', IU],
+    323: ['Vitamin E', mg, 20],
+    324: ['Vitamin D', IU, 400],
     325: ['Vitamin D2', Âµg],
     326: ['Vitamin D3', Âµg],
     337: ['Lycopene', Âµg],
@@ -50,18 +50,18 @@ let key = {
     341: ['Tocopherol, beta', mg],
     342: ['Tocopherol, gamma', mg],
     343: ['Tocopherol, delta', mg],
-    401: ['Vitamin C', mg],
-    404: ['Thiamin', mg],
-    405: ['Riboflavin', mg],
-    406: ['Niacin', mg],
-    410: ['Pantothenic acid', mg],
-    415: ['Vitamin B-6', mg],
-    417: ['Folate', Âµg],
-    418: ['Vitamin B-12', Âµg],
-    421: ['Choline', mg],
+    401: ['Vitamin C', mg, 60],
+    404: ['Thiamin', mg, 1.5],
+    405: ['Riboflavin', mg, 1.7],
+    406: ['Niacin', mg, 200],
+    410: ['Pantothenic acid', mg, 10],
+    415: ['Vitamin B-6', mg, 2],
+    417: ['Folate', Âµg, 400],
+    418: ['Vitamin B-12', Âµg, 6],
+    421: ['Choline', mg, 550],
     428: ['Menaquinone-4', Âµg],
     429: ['Dihydrophylloquinone', Âµg],
-    430: ['Vitamin K', Âµg],
+    430: ['Vitamin K', Âµg, 80],
     431: ['Folic acid', Âµg],
     454: ['Betaine', mg],
     501: ['Tryptophan', g],
@@ -97,12 +97,21 @@ export default function fullNutrientParser(apiResponse){
     foodArr.forEach(item => {
       let nutrients = item.full_nutrients;
       item.parsed_nutrients = {};
+      let daily_values = [];
+      item.daily_values = daily_values.sort((a,b) => a.percentDV > b.percentDV)
       return nutrients.forEach(nutrient => {
         if (key[nutrient.attr_id]){
           let parsed = item.parsed_nutrients;
           let nutrientObj = {}
           nutrientObj.quantity = nutrient.value;
           nutrientObj.unit = key[nutrient.attr_id][1]
+          if (key[nutrient.attr_id][2]) {
+            let nutrient = {};
+            nurient.name = key[nutrient.attr_id];
+            nutrient.rdv = key[nutrient.attr_id][2];
+            nutrient.percentDV = Math.floor(+nutrient.value/key[nutrient.attr_id][2]*100)
+            daily_values.push(nutrient)
+          }
           parsed[key[nutrient.attr_id][0]] = nutrientObj;
         }
       })
