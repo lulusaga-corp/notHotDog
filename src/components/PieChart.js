92e229a73f27110a1572ed55ea5e30ca9b718585
lucysutton91 <lucy.g.sutton@gmail.com>
2017-11-29 15:15:11 -0500

    styling the legend

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Text, StyleSheet, View, ScrollView, Dimensions } from "react-native";
 import { VictoryPie, VictoryLegend } from "victory-native";
 import { Svg } from "react-native-svg";
+import { Icon, List, ListItem, Card } from "react-native-elements";
 
 class PieChart extends Component {
   render() {
@@ -15,10 +16,20 @@ class PieChart extends Component {
     return (
     
       <View style={styles.container}>
-        <Svg width={Dimensions.get("window").width} height={130}>
+        
+      <VictoryPie
+      colorScale={["tomato", "orange", "gold"]}
+      data={chartData}
+      innerRadius={100}
+      padAngle={2}
+      style={{ labels: { fontSize: 1 } }}
+      />
+      {/* <Text style={styles.title}>{food.food_name}</Text> */}
+      {/* <Svg width={Dimensions.get("window").width} height={130}>
           <VictoryLegend x={100} y={100}
           centerTitle
           orientation="horizontal"
+          itemsPerRow={3}
           gutter={10}
           data={[
             { name: "Carbs", symbol: { fill: "tomato" } },
@@ -26,15 +37,19 @@ class PieChart extends Component {
             { name: "Protein", symbol: { fill: "gold" } }
           ]}
         />
-      </Svg>
-      <VictoryPie
-      colorScale={["tomato", "orange", "gold"]}
-      data={chartData}
-      innerRadius={2}
-      padAngle={2}
-      style={{ labels: { fontSize: 1 } }}
-      />
-      {/* <Text style={styles.title}>{food.food_name}</Text> */}
+      </Svg> */}
+        <Card title="Source of Calories" containerStyle={styles.legend}>
+          <Icon name="label" color="tomato"/><Text>Carbs</Text>
+          <Icon name="label" color="orange"/><Text>Fat</Text>
+          <Icon name="label" color="gold"/><Text>Protein</Text>
+        </Card>
+        {/* <Card title="Source of Calories">
+        <List>
+              <ListItem key="carbs" title="Carbs" leftIcon={{name: "label", color:"tomato"}}/>
+              <ListItem key="fat" title="Fat" leftIcon={{name: "label", color:"orange"}}/>
+              <ListItem key="protein" title="Protein" leftIcon={{name: "label", color:"gold"}}/>
+        </List>
+        </Card> */}
       </View>
     );
   }
@@ -52,6 +67,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     fontSize: 18,
     fontWeight: "bold",
+  },
+  lenged: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    // justifyContent: 'flex-start',
+    // alignContent: 'flex-start',
+    // alignSelf: 'flex-start',
+    flexWrap: 'nowrap'
   }
   
 });
--- src/scenes/SingleHistoryView.js
+++ src/scenes/SingleHistoryView.js
@@ -1,20 +1,57 @@
-import React, { Component } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-import PieChart from '../components/PieChart';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import {StyleSheet, View, ScrollView } from "react-native";
+import PieChart from "../components/PieChart";
+import { getAllUserMeals } from '../store/food';
+import { connect } from 'react-redux'
+import { ButtonGroup } from "react-native-elements";
 
 class SingleHistoryView extends Component {
-  render(props) {
-    //get the timestamp going
-    const singleMeal = this.props.mealInstance
+  constructor (props) {
+    super(props)
+    this.state = {
+      selectedIndex: 0,
+    }
+    this.updateIndex = this.updateIndex.bind(this)
+  }
+
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex})
+  }
+  componentWillMount () {
+     this.props.userId && this.props.fetchAllMeals(this.props.userId)
+  }
+
+  render() {
+    const singleMeal = this.props.mealInstance;
+    const { selectedIndex } = this.state
+    let reduced = singleMeal ? singleMeal.reduce((acc, foodItem) =>{
+      acc.serving += foodItem.serving
+      acc.data.protein += foodItem.data.protein
+      acc.data.carbs += foodItem.data.carbs
+      acc.data.fat += foodItem.data.fat
+      return acc
+    }, { serving: 0, data: { protein: 0, carbs: 0, fat: 0 }}) : null
+    let buttons = ["meal"]
+    let meal = [reduced]
+    singleMeal && singleMeal.map((food) => {
+      buttons.push(food.food_name)
+      meal.push(food)
+    })
     return (
-      <View style={styles.container}>
-      <Text>SingleHistoryView</Text>
-        {singleMeal ? (
-          <PieChart allFoods={singleMeal} />
-        ) : null}
-      </View>
-    );
+      <ScrollView style={styles.container}>
+        <ButtonGroup
+          onPress={this.updateIndex}
+          selectedIndex={selectedIndex}
+          buttons={buttons}
+          containerStyle={{height: 40}}
+          selectedTextStyle={{color: "#ef4836"}} />
+        <View>
+          {
+            singleMeal && <PieChart allFoods={meal[selectedIndex]} />
+          }
+        </View>
+      </ScrollView>
+    )
   }
 }
 
@@ -22,10 +59,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   }
+})
+
+const mapStateToProps = state => ({
+  mostRecent: state.food.mostRecent,
+  userId: state.auth && state.auth.user ? state.auth.user.uid : null
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchAllMeals: userId => dispatch(getAllUserMeals(userId))
 });
 
-mapStateToProps = state => ({
-  mostRecent: state.food.mostRecent
-});
-
-export default connect(mapStateToProps)(SingleHistoryView);
+export default connect(mapStateToProps, mapDispatchToProps)(SingleHistoryView);
